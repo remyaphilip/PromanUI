dc8db2c581af298281f49cc6539ab25237c15e9a
Remya Philipose <remyaphilipose@gmail.com>
2018-04-14 13:16:14 +1000

    commit 3

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -21,7 +21,7 @@ export class ProjectComponent implements OnInit,AfterViewInit {
   ngAfterViewInit() {
     //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
     //Add 'implements AfterViewInit' to the class.
-    this.sidenavprjt = this.sidenavComponent.curtrgt;
+    //this.sidenavprjt = this.sidenavComponent.curtrgt;
   }
   userId;
   project: Project[];
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,11 +1,13 @@
-import { Component, EventEmitter, OnInit, Input, Output} from '@angular/core';
+import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
 import { Nav } from './nav';
+import { LoginService } from "../login.service";
+import { Project } from "../interface/project";
 import { link } from '../navbar/link'
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss'],
-  
+  styleUrls: ['./sidenav.component.scss']
+
 })
 
 export class SidenavComponent implements OnInit {
@@ -15,36 +17,65 @@ export class SidenavComponent implements OnInit {
     , new link('Project', 'project')
     , new link('Issue', 'issue')
     , new link('Group', 'group')
-    ,new link('invite', 'invite')
+    , new link('invite', 'invite')
   ];
 
 
-  @Input() navs: Nav[] = [];
-  @Input() sidenavlistitem: Nav[] = [];
-  @Output() onClick = new EventEmitter<boolean>();
-  hideme: Array<boolean> = [false];
-  curtrgt: string;
+  projectList: Nav[] = [];
+  project: Project[];
+  projectName: string[] = [];
+  projectListStat: boolean = false;
+
+
+  // @Input() navs: Nav[] = [];
+  // @Input() sidenavlistitem: Nav[] = [];
+  // @Output() onClick = new EventEmitter<boolean>();
+  // hideme: Array<boolean> = [false];
+  // curtrgt: string;
 
 
 
-  vote(i: number, curtargt: string) {
-    this.curtrgt = curtargt
-    this.hideme = [false];
-    console.log(this.hideme);
-    this.hideme[i] = true;
-    console.log(this.hideme, curtargt);
-    this.onClick.emit(true);
+  // vote(i: number, curtargt: string) {
+  //   this.curtrgt = curtargt
+  //   this.hideme = [false];
+  //   console.log(this.hideme);
+  //   this.hideme[i] = true;
+  //   console.log(this.hideme, curtargt);
+  //   this.onClick.emit(true);
 
-  }
+  // }
 
 
-  constructor() {
-    console.log(this.sidenavlistitem);
+  constructor(public _loginService: LoginService) {
+    this.getProject(2);
+  //  console.log(this.projectList);
   }
 
   ngOnInit() {
-    console.log(this.hideme);
+    
+    //console.log(this.hideme);
   }
+
+  onClick(){
+    this.projectListStat = true;
+    
+  }
+
+ getProject(userId: number) {
+    this._loginService.getProjectPerUser(userId)
+      .subscribe(response => {
+        this.project = <Project[]>response;
+        this.project.forEach(
+          (item: Project, index) => {
+            this.projectList.push({ name: item.name });
+            this.projectName.push(item.name);
+          }
+        );
+      });
+      console.log(this.projectName);
+  }
+
+
 
 }
 
